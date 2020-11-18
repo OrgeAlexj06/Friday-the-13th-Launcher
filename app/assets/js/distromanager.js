82e56748d903dd4d2da762c6140d8c28cebb9b07
OrgeAlexj2006 <orgealexj06@gmail.com>
2020-11-18 21:23:55 +0100

    Mise à jour 1.2

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const distroURL = 'http://download1655.mediafire.com/hf85i4q2ysdg/8cmip5c9ov5db7h/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
